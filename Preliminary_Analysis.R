1180b42be6220cad141a9d739766cf57c0e45010
ijyliu <ijyliu@uchicago.edu>
2021-05-01 20:54:16 -0400

    will need to spend a TON of time with the codebook

--- Preliminary_Analysis.R
+++ Preliminary_Analysis.R
@@ -2,7 +2,7 @@
 # Do some summary statistics on VDem
 
 library(pacman)
-p_load('tidyverse', 'stargazer', 'estimatr', 'plm')
+p_load('tidyverse', 'stargazer', 'estimatr', 'plm', 'factoextra')
 
 # Start with a very small sample read-in for speed
 VDem <- read_csv('~/../Box/ECMA-31330-Project/V-Dem-CY-Full+Others-v11.1.csv')
@@ -26,3 +26,24 @@ FE_instab_turnout <- plm(e_wbgi_pve ~ v2eltrnout, data = VDem, index = c("countr
 # Get cluster SEs
 summary(FE_instab_turnout, cluster="country_name") 
 stargazer(FE_instab_turnout, se=list(coef(summary(FE_instab_turnout, cluster = c("country_name")))[, 2]), title="Fixed Effects Regression of WB Political Stability on Election Voter Turnout", covariate.labels = c("Election Turnout"), dep.var.labels = "WB Political Stability", dep.var.caption = "", align=TRUE, out="~/../repo/ECMA-31330-Project/Output/FE_Instab_Turnout_Reg.tex", omit.stat=c("f", "ser", "adj.rsq"), notes = "Includes country and time fixed effects. Country-clustered standard errors in parentheses.")
+
+# Principal Components Analysis
+# Source: http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/118-principal-component-analysis-in-r-prcomp-vs-princomp/
+# Let's do some exploratory work as to what are the major patterns in the data
+# We need to pick some numeric variables along interesting dimensions
+# I chose the democracy indices (codebook section 2), suffrage indicators (codebook 3.1.1)
+pca_results <- VDem %>%
+    select(starts_with("v2x")) %>%
+    prcomp(scale = TRUE)
+# Visualize the eigenvectors/display the percentage of variance explained by each principal component
+pdf("Output/Variance_Explained.pdf")
+print(fviz_eig(pca_results))
+dev.off()
+# Visualize the directions
+pdf("Output/Contributions_Directions.pdf")
+print(fviz_pca_var(pca_results,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+             ))
+dev.off()
