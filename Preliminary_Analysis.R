ac42580b7684a4ea9c2aff97e85d63fc179f6584
ijyliuC <ijyliu@uchicago.edu>
2021-05-01 21:22:52 -0500

    starter run on cluster

--- Preliminary_Analysis.R
+++ Preliminary_Analysis.R
@@ -8,8 +8,12 @@ if ('pacman' %in% rownames(installed.packages()) == FALSE) {
 library(pacman)
 p_load('tidyverse', 'stargazer', 'estimatr', 'plm', 'factoextra')
 
+# Detect whether this is a local run or on a computing cluster and set the file path appropriately
+data_dir <- ifelse(dir.exists("~/../Box/ECMA-31330-Project"), "~/../Box/ECMA-31330-Project", "~/Box/ECMA-31330-Project")
+
 # Start with a very small sample read-in for speed
-VDem <- read_csv('~/../Box/ECMA-31330-Project/V-Dem-CY-Full+Others-v11.1.csv')
+VDem <- read_csv(file.path(data_dir, '/V-Dem-CY-Full+Others-v11.1.csv'))
+print("Loaded in V-Dem data.")
 
 # Let us consider the relationship between political instability and voter turnout
 # One measure in V-Dem is the WB World Governance Indicators politcal stability index. Higher = more stable. This is variable e_wbgi_pve.
@@ -19,17 +23,17 @@ VDem <- read_csv('~/../Box/ECMA-31330-Project/V-Dem-CY-Full+Others-v11.1.csv')
 VDem %>%
     select(e_wbgi_pve, v2eltrnout) %>%
     data.frame() %>%
-    stargazer(out="~/../repo/ECMA-31330-Project/Output/Summary_Statistics.tex", title="Summary Statistics", covariate.labels = c("WB Political Stability", "Election Turnout"))
+    stargazer(out="Output/Summary_Statistics.tex", title="Summary Statistics", covariate.labels = c("WB Political Stability", "Election Turnout"))
 
 # OLS Regression
 OLS_instab_turnout <- lm(e_wbgi_pve ~ v2eltrnout, data = VDem)
-stargazer(OLS_instab_turnout, se = starprep(OLS_instab_turnout, se_type = "stata"), title="Regression of WB Political Stability on Election Voter Turnout", covariate.labels = c("Election Turnout"), dep.var.labels = "WB Political Stability", dep.var.caption = "", align=TRUE, out="~/../repo/ECMA-31330-Project/Output/Instab_Turnout_Reg.tex", omit.stat=c("f", "ser", "adj.rsq"), notes = "Robust standard errors in parentheses.")
+stargazer(OLS_instab_turnout, se = starprep(OLS_instab_turnout, se_type = "stata"), title="Regression of WB Political Stability on Election Voter Turnout", covariate.labels = c("Election Turnout"), dep.var.labels = "WB Political Stability", dep.var.caption = "", align=TRUE, out="Output/Instab_Turnout_Reg.tex", omit.stat=c("f", "ser", "adj.rsq"), notes = "Robust standard errors in parentheses.")
 
 # Country-Year Fixed Effects Regression
 FE_instab_turnout <- plm(e_wbgi_pve ~ v2eltrnout, data = VDem, index = c("country_name", "year"), model = "within")
 # Get cluster SEs
 summary(FE_instab_turnout, cluster="country_name") 
-stargazer(FE_instab_turnout, se=list(coef(summary(FE_instab_turnout, cluster = c("country_name")))[, 2]), title="Fixed Effects Regression of WB Political Stability on Election Voter Turnout", covariate.labels = c("Election Turnout"), dep.var.labels = "WB Political Stability", dep.var.caption = "", align=TRUE, out="~/../repo/ECMA-31330-Project/Output/FE_Instab_Turnout_Reg.tex", omit.stat=c("f", "ser", "adj.rsq"), notes = "Includes country and time fixed effects. Country-clustered standard errors in parentheses.")
+stargazer(FE_instab_turnout, se=list(coef(summary(FE_instab_turnout, cluster = c("country_name")))[, 2]), title="Fixed Effects Regression of WB Political Stability on Election Voter Turnout", covariate.labels = c("Election Turnout"), dep.var.labels = "WB Political Stability", dep.var.caption = "", align=TRUE, out="Output/FE_Instab_Turnout_Reg.tex", omit.stat=c("f", "ser", "adj.rsq"), notes = "Includes country and time fixed effects. Country-clustered standard errors in parentheses.")
 
 # Principal Components Analysis
 # Source: http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/118-principal-component-analysis-in-r-prcomp-vs-princomp/
