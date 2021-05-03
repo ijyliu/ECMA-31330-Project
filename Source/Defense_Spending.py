a92b7b648441f0e4bae52fd56e2c0af2ad805684
ijyliu <ijyliu@uchicago.edu>
2021-05-03 19:51:30 -0400

    more missing values analysis

--- Source/Defense_Spending.py
+++ Source/Defense_Spending.py
@@ -28,8 +28,6 @@ sipri_milex_per_gdp = (pd.read_excel(data_dir + "/SIPRI-Milex-data-1949-2020_0.x
                          .transpose()
                          .rename_axis('Year'))
 
-print(sipri_milex_per_gdp)
-
 # Here's a internally/interpolated version of the data
 sipri_milex_per_gdp_interpolate = (sipri_milex_per_gdp.interpolate(limit_area='inside'))
 
@@ -37,11 +35,11 @@ sipri_milex_per_gdp_interpolate_90s_on = sipri_milex_per_gdp_interpolate.query('
 
 # For every year, plot the share of missing values
 sns.heatmap(sipri_milex_per_gdp.isnull(), cbar=False)
-plt.savefig(figures_dir + "/Missing_Values.pdf")
+plt.savefig(figures_dir + "/SIPRI_Missing_Values.pdf")
 sns.heatmap(sipri_milex_per_gdp_interpolate.isnull(), cbar=False)
-plt.savefig(figures_dir + "/Missing_Values_Interpolate.pdf")
+plt.savefig(figures_dir + "/SIPRI_Missing_Values_Interpolate.pdf")
 sns.heatmap(sipri_milex_per_gdp_interpolate_90s_on.isnull(), cbar=False)
-plt.savefig(figures_dir + "/Missing_Values_Interpolate_Post_1990.pdf")
+plt.savefig(figures_dir + "/SIPRI_Missing_Values_Interpolate_Post_1990.pdf")
 
 exit()
 
