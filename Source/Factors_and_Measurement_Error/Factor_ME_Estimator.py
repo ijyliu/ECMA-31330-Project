ce9bef9b2c0c5354b2cd6d0eddb6956b8f8ebf31
ijyliu <ijyliu@uchicago.edu>
2021-05-08 14:01:46 -0400

    standardize beforehand

--- Source/Factors_and_Measurement_Error/Factor_ME_Estimator.py
+++ Source/Factors_and_Measurement_Error/Factor_ME_Estimator.py
@@ -1,21 +1,17 @@
 # Factor_ME_Estimator.py
-# Calculate the factor estimator and benchmark it against IV
+# Function for the PCR estimator
+# Please standardize the y and X beforehand
 
-from sklearn.preprocessing import StandardScaler
 from pca import pca
 import statsmodels.api as sm
 
 def PCR_coeffs(y, X):
 
-    # Standardize
-    y_std = StandardScaler().fit_transform(y)
-    X_std = StandardScaler().fit_transform(X)
-
     # Perform the factor analysis
     pca_model = pca()
-    pca_results = pca_model.fit_transform(X_std)
+    pca_results = pca_model.fit_transform(X)
 
-    # Regress y, life_expectancy, on the first principal component and output the results
+    # Regress on the first principal component and output the results
     factor_regression = sm.OLS(y, pca_results['PC'].iloc[:, 0].reset_index(drop = True)).fit()
 
     return(factor_regression.params)
