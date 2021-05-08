af5c1473b21d2d27c67b0c0367c9c7618b57b4f0
ijyliu <ijyliu@uchicago.edu>
2021-05-07 21:35:29 -0400

    start on world bank pca

--- Source/Factors_and_Measurement_Error/application_gdp_life_expectancy.py
+++ Source/Factors_and_Measurement_Error/application_gdp_life_expectancy.py
@@ -1,12 +1,32 @@
 
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
 
 data_dir = "~/Box/ECMA-31330-Project"
 
 # Load in the data
-wb_data = pd.read_csv(data_dir + "/WB_Data.csv")
+wb_data = (pd.read_csv(data_dir + "/WB_Data.csv", index_col=['economy', 'series'])
+             # Reshape and create a balanced panel
+             .transpose()
+             .stack(level = 'economy')
+             .rename_axis(None, axis = 1)
+             .rename({"SP.DYN.LE00.IN":"life_exp", "NY.GDP.PCAP.PP.CD":"gdp_pc", "NY.GNP.PCAP.PP.CD":"gnp_pc", "SI.SPR.PCAP":"survey_inc_con_pc", "SL.GDP.PCAP.EM.KD":"gdp_per_emp"})
+             .rename_axis(['year','country'])
+             # Sort by country for interpolation
+             .sort_index(level=['country', 'year'])
+             # Linear interpolation
+             .interpolate(limit_area='inside')
+             .dropna()
+             )
+
+print(wb_data)
+
+X = wb_data.iloc[:, 0:3].to_numpy()
+y = wb_data['SP.DYN.LE00.IN'].to_numpy()
 
 # Perform the factor analysis
+pca = PCA(n_components=1)
+X = pca.fit_transform(X)
 
 # Conduct instrumental variables regression
--- Source/Factors_and_Measurement_Error/get_wbdata.py
+++ Source/Factors_and_Measurement_Error/get_wbdata.py
@@ -7,6 +7,6 @@ data_dir = "~/Box/ECMA-31330-Project"
 
 # Load in some series
 # Life expectancy at birth (total), GDP PC international PPP, GNI PC PPP, Survey Mean Income/Consumption Per Capita, ILO GDP per person employed, Net Foreign Assets Per Capita
-wb_data = wb.data.DataFrame(['SP.DYN.LE00.IN', 'NY.GDP.PCAP.PP.CD', 'NY.GNP.PCAP.PP.CD', 'SI.SPR.PCAP', 'SL.GDP.PCAP.EM.KD', 'NW.NFA.PC'])
+wb_data = wb.data.DataFrame(['SP.DYN.LE00.IN', 'NY.GDP.PCAP.PP.CD', 'NY.GNP.PCAP.PP.CD', 'SI.SPR.PCAP', 'SL.GDP.PCAP.EM.KD', 'NW.NFA.PC'], skipAggs=True, numericTimeKeys=True)
 
 wb_data.to_csv(data_dir + "/WB_Data.csv")
