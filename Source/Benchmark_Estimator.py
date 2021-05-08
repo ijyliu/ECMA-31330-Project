2d94ea0d806cfab9850bb2315d947ee58ddf4cee
ijyliu <ijyliu@uchicago.edu>
2021-05-08 17:59:56 -0400

    estimator calculation function

--- Source/Benchmark_Estimator.py
+++ Source/Benchmark_Estimator.py
@@ -13,20 +13,31 @@ import matplotlib
 matplotlib.use('pdf')
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
+from linearmodels import IV2SLS
 
 # Load in the DGP function
 from Simulate_DGP import *
 
-DGP(100, 0.9, 3, 0.9, [1,0,0], [1,1,1])
-
-data_dir = "~/Box/ECMA-31330-Project"
-
-sim_data = pd.read_csv(data_dir + "/ME_Sim.csv")
-
-# Standardize
-std_sim_data = pd.DataFrame(StandardScaler().fit_transform(sim_data), index=sim_data.index, columns=sim_data.columns)
+# Load in the PCR/Factor estimator
+from Factor_ME_Estimator import *
 
 # Given some simulation parameters, run both the PCA regression and the IV regression for a simulation
-def benchmark_estimator(N, rho, p, kappa, beta, x_measurement_errors, num_sims):
+def get_estimators(N, rho, p, kappa, beta, x_measurement_errors):
 
+    # Run the DGP
     Y, true_X, mismeasured_X, Z = DGP(N, rho, p, kappa, beta, x_measurement_errors)
+
+    # Standardize
+    Y = StandardScaler().fit_transform(Y)
+    true_X = StandardScaler().fit_transform(true_X)
+    mismeasured_X = StandardScaler().fit_transform(mismeasured_X)
+    Z = StandardScaler().fit_transform(Z)
+
+    # Calculate estimators
+    beta_OLS_true = sm.OLS(Y, true_X[:, 0]).fit().params
+    beta_OLS_mismeasured = sm.OLS(Y, mismeasured_X[:, 0]).fit().params
+    beta_PCR = PCR_coeffs(Y, mismeasured_X)
+    beta_IV = IV2SLS(dependent = Y, endog = mismeasured_X[:, 0], instruments = Z).fit().params
+
+    return(beta_OLS_true, beta_OLS_mismeasured, beta_PCR, beta_IV)
