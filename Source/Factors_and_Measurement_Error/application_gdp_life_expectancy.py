81b097d71c89d695693e3da918d172eb3b842e4e
ijyliu <ijyliu@uchicago.edu>
2021-05-07 21:38:13 -0400

    bugfixes

--- Source/Factors_and_Measurement_Error/application_gdp_life_expectancy.py
+++ Source/Factors_and_Measurement_Error/application_gdp_life_expectancy.py
@@ -11,19 +11,18 @@ wb_data = (pd.read_csv(data_dir + "/WB_Data.csv", index_col=['economy', 'series'
              .transpose()
              .stack(level = 'economy')
              .rename_axis(None, axis = 1)
-             .rename({"SP.DYN.LE00.IN":"life_exp", "NY.GDP.PCAP.PP.CD":"gdp_pc", "NY.GNP.PCAP.PP.CD":"gnp_pc", "SI.SPR.PCAP":"survey_inc_con_pc", "SL.GDP.PCAP.EM.KD":"gdp_per_emp"})
+             .rename(columns = {"SP.DYN.LE00.IN":"life_exp", "NY.GDP.PCAP.PP.CD":"gdp_pc", "NY.GNP.PCAP.PP.CD":"gnp_pc", "SI.SPR.PCAP":"survey_inc_con_pc", "SL.GDP.PCAP.EM.KD":"gdp_per_emp"})
              .rename_axis(['year','country'])
              # Sort by country for interpolation
              .sort_index(level=['country', 'year'])
              # Linear interpolation
              .interpolate(limit_area='inside')
-             .dropna()
-             )
+             .dropna())
 
 print(wb_data)
 
-X = wb_data.iloc[:, 0:3].to_numpy()
-y = wb_data['SP.DYN.LE00.IN'].to_numpy()
+X = wb_data.drop(columns = 'life_exp').to_numpy()
+y = wb_data['life_exp'].to_numpy()
 
 # Perform the factor analysis
 pca = PCA(n_components=1)
