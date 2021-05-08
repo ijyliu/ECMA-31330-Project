b3401071266cb1d7898ee5aaead6217b970d2da7
ijyliu <ijyliu@uchicago.edu>
2021-05-07 20:40:56 -0400

    script to fetch wb data

--- Source/Factors_and_Measurement_Error/application_gdp_life_expectancy.py
+++ Source/Factors_and_Measurement_Error/application_gdp_life_expectancy.py
@@ -1,10 +1,12 @@
 
 import numpy as np
 import pandas as pd
-import wbgapi as wb
 
-# Load in some series
-# Life expectancy at birth (total), GDP PC international PPP, GNI PC PPP, Survey Mean Income/Consumption Per Capita, ILO GDP per person employed, Net Foreign Assets Per Capita
-wb_data = wb.data.DataFrame(['SP.DYN.LE00.IN', 'NY.GDP.PCAP.PP.CD', 'NY.GNP.PCAP.PP.CD', 'SI.SPR.PCAP', 'SL.GDP.PCAP.EM.KD', 'NW.NFA.PC'])
+data_dir = "~/Box/ECMA-31330-Project"
 
-print(wb_data)
+# Load in the data
+wb_data = pd.read_csv(data_dir + "/WB_Data.csv")
+
+# Perform the factor analysis
+
+# Conduct instrumental variables regression
