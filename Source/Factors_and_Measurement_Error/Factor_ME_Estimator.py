7001e93b9dfee31f94c4ff65b75654694f3ff96e
ijyliu <ijyliu@uchicago.edu>
2021-05-08 13:57:11 -0400

    estimator draft

--- Source/Factors_and_Measurement_Error/Factor_ME_Estimator.py
+++ Source/Factors_and_Measurement_Error/Factor_ME_Estimator.py
@@ -1,2 +1,21 @@
 # Factor_ME_Estimator.py
 # Calculate the factor estimator and benchmark it against IV
+
+from sklearn.preprocessing import StandardScaler
+from pca import pca
+import statsmodels.api as sm
+
+def PCR_coeffs(y, X):
+
+    # Standardize
+    y_std = StandardScaler().fit_transform(y)
+    X_std = StandardScaler().fit_transform(X)
+
+    # Perform the factor analysis
+    pca_model = pca()
+    pca_results = pca_model.fit_transform(X_std)
+
+    # Regress y, life_expectancy, on the first principal component and output the results
+    factor_regression = sm.OLS(y, pca_results['PC'].iloc[:, 0].reset_index(drop = True)).fit()
+
+    return(factor_regression.params)
