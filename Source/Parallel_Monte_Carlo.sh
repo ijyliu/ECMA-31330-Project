49edf4e762d08069981bd0dc06f0feba2004cd85
ijyliu <ijyliu@uchicago.edu>
2021-05-09 17:06:13 -0400

    parallel code edits

--- Source/Parallel_Monte_Carlo.sh
+++ Source/Parallel_Monte_Carlo.sh
@@ -6,12 +6,10 @@
 #SBATCH --output=/dev/null
 
 # Get the array end
-readarray -d '' filenames < <(find /project/burning_glass/Input/'Burning Glass Data'/Main -name "*.txt" -print0)
-let arrayend=${#filenames[@]}-1
-
-ls | sed -e s/[^0-9]//g
+readarray -d '' filename < <(find ~/Box/ECMA-31330-Project -name "*_parameters.csv" -printf '%f\n')
+let arrayend=${filename//[^0-9]/}-1
 
 module load python/booth/3.6/3.6.12
 FIRSTJOBID=$(sbatch --parsable Setup_Simulations.sh)
 SECONDJOBID=$(sbatch --parsable --dependency=afterany:$FIRSTJOBID --array=0-$((arrayend)) Parallel_Run_Simulations.sh)
-sbatch --parsable --dependency=afterok:$SECONDJOBID Cl_File_Cleanup.sh
+sbatch --parsable --dependency=afterok:$SECONDJOBID Parallel_Compile_Results.sh
