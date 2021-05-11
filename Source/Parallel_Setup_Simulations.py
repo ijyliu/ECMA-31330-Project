9bb277aa94263d0e4c26bb97cd21a5accdeca937
ijyliu <ijyliu@uchicago.edu>
2021-05-10 20:27:25 -0400

    revise parallel code

--- Source/Parallel_Setup_Simulations.py
+++ Source/Parallel_Setup_Simulations.py
@@ -6,40 +6,19 @@ from ME_Setup import *
 
 # Packages
 import os
-import pandas as pd
 import glob
 
 # Simulations dataframe with variations of parameter values
+# num_sims
 Ns = [100, 1000]
-rhos = [0.8, 0.9]
-ps = [3]
-kappas = [0.1, 0.9]
+betas = [1, 10]
+# The numpy arrays of measurement error values have to be written out as strings for storage and converted later
+me_means = ['[0,0]', '[1, 0]']
+me_covs = ['[[100, 0], [0, 1]]', '[[1, 0], [0, 1]]']
+kappas = [1]
 
-# This is an awkward/bad way of making the combos of lists of coefficients
-betas = ['beta_combo_1', 'beta_combo_2']
-mes = ['me_combo_1', 'me_combo_2']
-
-def assign_beta(beta_combo):
-    if beta_combo == 'beta_combo_1':
-        return([1, 1, 1])
-    if beta_combo == 'beta_combo_2':
-        return([2, 1, 1])
-
-def assign_me(me_combo):
-    if me_combo == 'me_combo_1':
-        return([10, 0, 0])
-    if me_combo == 'me_combo_2':
-        return([2, 0, 0])
-
-# Cartesian product of scenarios
-index = pd.MultiIndex.from_product([Ns, rhos, ps, kappas, betas, mes], names = ["N", "rho", "p", "kappa", "beta", "me"])
-
-# Produce scenarios dataframe
-scenarios = pd.DataFrame(index = index).reset_index()
-
-# Convert the combo strings into lists
-scenarios['beta_list'] = scenarios.apply(lambda x: assign_beta(x.beta), axis = 1)
-scenarios['me_list'] = scenarios.apply(lambda x: assign_me(x.me), axis = 1)
+# Scenarios dataframe
+scenarios = produce_scenarios_cartesian(Ns, betas, me_means, me_covs, kappas)
 
 # Remove any pre-existing parameter combinations file
 files_to_remove = glob.glob(data_dir + '/*_parameter_combos.csv')
