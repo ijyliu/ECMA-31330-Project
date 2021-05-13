59f4552af075626385eb4ca5d14be73fffe56360
ijyliu <ijyliu@uchicago.edu>
2021-05-12 21:17:23 -0400

    more covariates but still a weak result

--- Source/Application/Application_Health_Spending_Share_LE.py
+++ Source/Application/Application_Health_Spending_Share_LE.py
@@ -26,10 +26,12 @@ wb_data = (pd.read_csv(input_dir + "/WB_Data.csv", index_col=['economy', 'series
              .transpose()
              .stack(level = 'economy')
              .rename_axis(None, axis = 1)
-             .rename(columns = {"SP.DYN.LE00.IN":"life_exp", "NY.GDP.PCAP.PP.CD":"gdp_pc", "NY.GNP.PCAP.PP.CD":"gnp_pc", "SI.SPR.PCAP":"survey_inc_con_pc", "SL.GDP.PCAP.EM.KD":"gdp_per_emp", "SH.XPD.GHED.CH.ZS":"govt_health_share_wb"})
+             .rename(columns = {"SP.DYN.LE00.IN":"life_exp", "SH.XPD.GHED.CH.ZS":"govt_health_share_wb", "NY.GDP.PCAP.PP.CD":"gdp_pc_ppp", "NY.GNP.PCAP.PP.CD":"gnp_pc_ppp", "SI.SPR.PCAP":"survey_inc_con_pc", "SL.GDP.PCAP.EM.KD":"gdp_per_emp", 'NY.GDP.PCAP.CD':"gdp_pc_cd", 'NY.GNP.PCAP.CD':'gnp_pc_cd', 'SI.SPR.PC40':'survey_inc_con_pc_40', 'FI.RES.TOTL.CD':'tot_reserves', 'SI.POV.DDAY':'pov_hcr_ppp', 'SI.POV.NAHC':'nat_pov_hc', 'SI.POV.GAPS':'pov_gap'})
              .rename_axis(['year', 'country'])
              .reset_index()
              .astype({'year': 'datetime64[ns]', 'country': 'str'}))
+
+print(wb_data.columns)
 
 oecd_data = (pd.read_csv(input_dir + "/OECD_Govt_Share_Health_Spending.csv")
                .query('INDICATOR == "HEALTHEXP' and 'SUBJECT == "COMPULSORY' and 'MEASURE == "PC_HEALTH_EXP"' and 'FREQUENCY == "A"')
@@ -74,13 +76,13 @@ plt.savefig(figures_dir + "/LE_Health_Econ_Correlations.pdf")
 plt.close()
 
 # OLS for benchmark
-ols_benchmark = sm.OLS(std_data['life_exp'], std_data['mean_govt_health_share']).fit()
+ols_benchmark = smf.ols("life_exp ~ mean_govt_health_share", data = std_data.reset_index()).fit()
 
 # Many covariate OLS
-ols_many_covariates = sm.OLS(std_data['life_exp'], pd.concat([std_data['mean_govt_health_share'], std_data['gdp_pc'], std_data['gnp_pc'], std_data['survey_inc_con_pc'], std_data['gdp_per_emp']], axis = 1)).fit()
+ols_many_covariates = smf.ols("life_exp ~ mean_govt_health_share + gdp_pc_ppp + gnp_pc_ppp + survey_inc_con_pc + gdp_per_emp + gdp_pc_cd + gnp_pc_cd + survey_inc_con_pc_40 + tot_reserves + pov_hcr_ppp + nat_pov_hc + pov_gap", data = std_data.reset_index()).fit()
 
 # Panel Fixed Effects Regression for Benchmark
-fixed_effects_results = smf.ols("life_exp ~ mean_govt_health_share + gdp_pc + gnp_pc + survey_inc_con_pc + gdp_per_emp + C(year) + C(country)", data = std_data.reset_index()).fit(cov_type='cluster', cov_kwds={'groups': std_data.reset_index()['country']})
+fixed_effects_results = smf.ols("life_exp ~ mean_govt_health_share + gdp_pc_ppp + gnp_pc_ppp + survey_inc_con_pc + gdp_per_emp + gdp_pc_cd + gnp_pc_cd + survey_inc_con_pc_40 + tot_reserves + pov_hcr_ppp + nat_pov_hc + pov_gap + C(year) + C(country)", data = std_data.reset_index()).fit(cov_type='cluster', cov_kwds={'groups': std_data.reset_index()['country']})
 
 # Decompose into matrix for PCA analysis
 # This contains only the economic covariates
@@ -90,8 +92,12 @@ X = std_data.drop(columns = ['life_exp', 'mean_govt_health_share']).to_numpy()
 pca_model = pca()
 pca_results = pca_model.fit_transform(X)
 
+# Count number of PCs
+num_pcs = pca_results['PC'].shape[1]
+PC_names = ['PC' + str(i + 1) for i in range(num_pcs)]
+
 # Add PCA results to the dataframe
-std_data = pd.concat([std_data.reset_index(), pca_results['PC'].iloc[:, 0].reset_index(drop = True)], axis = 1, names = [std_data.columns, 'PC1'])
+std_data = pd.concat([std_data.reset_index(), pca_results['PC'].reset_index(drop = True)], axis = 1, names = [std_data.columns, PC_names])
 
 # Plot the loadings
 sns.heatmap(pca_results['loadings'], cmap='YlGnBu')
@@ -112,9 +118,10 @@ pc_fixed_effects_results = smf.ols("life_exp ~ mean_govt_health_share + PC1 + C(
 # Regression table settings
 reg_table = Stargazer([ols_benchmark, ols_many_covariates, fixed_effects_results, partial_pc_regression, pc_fixed_effects_results])
 reg_table.dependent_variable_name("Life Expectancy at Birth (Years)")
-reg_table.covariate_order(['mean_govt_health_share', 'gdp_pc', 'gnp_pc', 'survey_inc_con_pc', 'gdp_per_emp', 'PC1'])
-reg_table.rename_covariates({"mean_govt_health_share":"Government Share of Health Expenditure", "gdp_pc":"GDP Per Capita PPP", "gnp_pc":"GNP Per Capita PPP", "survey_inc_con_pc":"Survey Income/Consumption Per Capita", "gdp_per_emp":"GDP Per Employed Person"})
+reg_table.covariate_order(['mean_govt_health_share'])
+reg_table.rename_covariates({"mean_govt_health_share":"Govt. Share of Health Exp."})
 # Fixed effects indicator
+reg_table.add_line('Covariates', ['None', 'Econ Indicators', 'Econ Indicators', 'PCs', 'PCs'])
 reg_table.add_line('Fixed Effects', ['No', 'No', 'Yes', 'No', 'Yes'])
 reg_table.show_degrees_of_freedom(False)
 reg_table.add_custom_notes(["All variables are standardized."])
--- Source/Application/Get_WB_Data.py
+++ Source/Application/Get_WB_Data.py
@@ -11,7 +11,7 @@ from ME_Setup import *
 import wbgapi as wb
 
 # Load in some series
-# Life expectancy at birth (total), GDP PC international PPP, GNI PC PPP, Survey Mean Income/Consumption Per Capita, ILO GDP per person employed, Net Foreign Assets Per Capita, Healthcare spending by government as a share of total healthcare spending
-wb_data = wb.data.DataFrame(['SP.DYN.LE00.IN', 'NY.GDP.PCAP.PP.CD', 'NY.GNP.PCAP.PP.CD', 'SI.SPR.PCAP', 'SL.GDP.PCAP.EM.KD', 'NW.NFA.PC', 'SH.XPD.GHED.CH.ZS'], skipAggs=True, numericTimeKeys=True)
+# Life expectancy at birth (total), GDP PC international PPP, GDP PC Current USD, GNI PC PPP, GNI PC Atlas Method, Survey Mean Income/Consumption Per Capita, Survey mean inc/cons for the bottom 40% of the population, ILO GDP per person employed, Net Foreign Assets Per Capita, Total Reserves, Poverty HCR 2011 PPP percent pop, Poverty HCR at national lines, Poverty gap 2011 PPP, Healthcare spending by government as a share of total healthcare spending
+wb_data = wb.data.DataFrame(['SP.DYN.LE00.IN', 'NY.GDP.PCAP.PP.CD', 'NY.GDP.PCAP.CD', 'NY.GNP.PCAP.PP.CD', 'NY.GNP.PCAP.CD', 'SI.SPR.PCAP', 'SI.SPR.PC40', 'SL.GDP.PCAP.EM.KD', 'NW.NFA.PC', 'FI.RES.TOTL.CD', 'SI.POV.DDAY', 'SI.POV.NAHC', 'SI.POV.GAPS', 'SH.XPD.GHED.CH.ZS'], skipAggs=True, numericTimeKeys=True)
 
 wb_data.to_csv(input_dir + "/WB_Data.csv")
