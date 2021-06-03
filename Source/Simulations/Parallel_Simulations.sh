37827706a9f3752015e87bac4651f2fef35c8bcf
ijyliu <ijyliu@uchicago.edu>
2021-06-03 19:04:52 -0400

    add specs

--- Source/Simulations/Parallel_Simulations.sh
+++ Source/Simulations/Parallel_Simulations.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=Parallel_Simulations
-# Suppress output
+#SBATCH --partition=broadwl       # standard (default), long, gpu, mpi, highmem
+#SBATCH --cpus-per-task=1          # number of CPUs requested (for parallel tasks)
+#SBATCH --mem=1G           # requested memory
+#SBATCH --time=06:00:00          # wall clock limit (d-hh:mm:ss)
+#SBATCH --job-name=Parallel_Simulations   # user-defined job name
 #SBATCH --output=/dev/null
 
 # Get the array end
