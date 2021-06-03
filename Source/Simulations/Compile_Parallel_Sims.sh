ff11cbbbaff32349d20e0f0fd3fcc98d3902ae70
ijyliu <ijyliu@uchicago.edu>
2021-06-03 18:57:18 -0400

    more files

---
+++ Source/Simulations/Compile_Parallel_Sims.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --partition=broadwl       # standard (default), long, gpu, mpi, highmem
+#SBATCH --cpus-per-task=1          # number of CPUs requested (for parallel tasks)
+#SBATCH --mem=1G           # requested memory
+#SBATCH --time=06:00:00          # wall clock limit (d-hh:mm:ss)
+
+#SBATCH --job-name=Compile_Parallel_Sims   # user-defined job name
+
+#SBATCH --output=%x.out # output file name
+
+module load python
+python Compile_Parallel_Sims.py
---
+++ Source/Simulations/Setup_Parallel_Sims.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --partition=broadwl       # standard (default), long, gpu, mpi, highmem
+#SBATCH --cpus-per-task=1          # number of CPUs requested (for parallel tasks)
+#SBATCH --mem=1G           # requested memory
+#SBATCH --time=06:00:00          # wall clock limit (d-hh:mm:ss)
+
+#SBATCH --job-name=Setup_Parallel_Sims   # user-defined job name
+
+#SBATCH --output=%x.out # output file name
+
+module load python
+python Setup_Parallel_Sims.py
