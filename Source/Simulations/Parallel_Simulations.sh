c02381035964d9cc5dec5a71ddd9b84d5d66cf93
ijyliu <ijyliu@uchicago.edu>
2021-06-05 12:00:03 -0400

    edit .sh

--- Source/Simulations/Parallel_Simulations.sh
+++ Source/Simulations/Parallel_Simulations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=broadwl       # standard (default), long, gpu, mpi, highmem
+#SBATCH --partition=broadwl       # cluster partition
 #SBATCH --cpus-per-task=1          # number of CPUs requested (for parallel tasks)
 #SBATCH --mem=1G           # requested memory
 #SBATCH --time=06:00:00          # wall clock limit (d-hh:mm:ss)
